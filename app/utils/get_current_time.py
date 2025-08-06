9deafce1c2edc751b67918a1b05853a180ea8b5d
KinhNguyenVan <kinh30072004k@gmail.com>
2025-08-06 19:09:03 +0700

    update

--- app/utils/get_current_time.py
+++ app/utils/get_current_time.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from zoneinfo import ZoneInfo  
 
-def get_current_time_by_timezone():
+def get_current_time():
     now = datetime.now(ZoneInfo("Asia/Ho_Chi_Minh"))
     return now.strftime("%A, %d/%m/%Y %H:%M")
